5b7c818aed8db25e7eec4d542801a4c23e8273c3
Roland Tecson <roland@gameofapps.org>
2023-03-11 14:20:40 -0800

    Use Toast instead of println

--- app/src/main/java/com/example/bottomnavigation/FragmentFour.java
+++ app/src/main/java/com/example/bottomnavigation/FragmentFour.java
@@ -1,11 +1,13 @@
 package com.example.bottomnavigation;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -44,7 +46,8 @@ public class FragmentFour extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("Button four tapped.");
+                Context context = getActivity();
+                Toast.makeText(context, "Button 4 tapped", Toast.LENGTH_SHORT).show();
             }
         });
     }
