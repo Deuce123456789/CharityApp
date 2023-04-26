cabb887d66c90afdea77c92c10c1dd4ee1d3cf45
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2023-04-25 19:23:25 -0700

    better dataclass and half of store page

--- app/src/main/java/com/example/bottomnavigation/DataClass.java
+++ app/src/main/java/com/example/bottomnavigation/DataClass.java
@@ -5,4 +5,17 @@ import java.util.ArrayList;
 public class DataClass {
     public Integer NumTokens;
     public ArrayList<ItemDummy> StoreItems;
+
+    private static DataClass instance = null;
+
+    private DataClass() {
+        // constructor
+    }
+
+    public DataClass get_instance() {
+        if (instance == null) {
+            instance = new DataClass();
+        }
+        return instance;
+    }
 }
